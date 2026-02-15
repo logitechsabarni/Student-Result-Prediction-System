4e60dd517fd95dcf30d39dadd4c7f41c9a1a20ad
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2026-02-15 22:05:40 +0530

    Update predict.py

--- backend/predict.py
+++ backend/predict.py
@@ -1,11 +1,19 @@
 import joblib
 import pandas as pd
+import os
 
-reg_model = joblib.load("models/regression_model.pkl")
-clf_model = joblib.load("models/classification_model.pkl")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def predict_scores(input_df):
+reg_model_path = os.path.join(BASE_DIR, "models", "regression_model.pkl")
+clf_model_path = os.path.join(BASE_DIR, "models", "classification_model.pkl")
+
+reg_model = joblib.load(reg_model_path)
+clf_model = joblib.load(clf_model_path)
+
+def predict_scores(input_dict):
+    input_df = pd.DataFrame([input_dict])
+    
     reg_prediction = reg_model.predict(input_df)[0]
     clf_prediction = clf_model.predict(input_df)[0]
-
+    
     return reg_prediction, clf_prediction
